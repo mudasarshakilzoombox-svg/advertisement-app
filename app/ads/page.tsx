6427b775434b296cf3c5b14f2489770de2827e3e
Mudasar Shakil <mudasarshakil.zoombox@gmail.com>
2026-02-24 12:19:15 +0500

    padding added

--- app/ads/page.tsx
+++ app/ads/page.tsx
@@ -7,7 +7,7 @@ export default function AdsPage() {
   const initialAds = getRandomAds(9);
 
   return (
-    <main className="bg-gray-100 min-h-screen p-8">
+    <main className="bg-gray-100 min-h-screen p-14">
       <h1 className="text-3xl text-gray-900 font-bold mb-8">
         All Ads
       </h1>
